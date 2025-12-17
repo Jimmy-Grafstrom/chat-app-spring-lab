3beaeac87f58545db1badd878b375f2ccd691e9d
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2025-12-17 19:10:19 +0100

    commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,50 @@
 CREATE TABLE users
 (
     id       BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(20) NOT NULL UNIQUE ,
+    username VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE messages (
-                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          text TEXT,
-                          timestamp DATETIME (6),
-                          user_id BIGINT,
-                          FOREIGN KEY (user_id) REFERENCES users(id) on delete CASCADE
+CREATE TABLE messages
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    text      TEXT,
+    timestamp DATETIME(6),
+    user_id   BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users (id) on delete CASCADE
 );
 
-ALTER TABLE messages (
-    timestamp DATETIME(4)
-    );
+CREATE TABLE comment
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    text      TEXT,
+    timestamp DATETIME(6),
+    user_id   BIGINT NOT NULL,
+    message_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE
+);
+
+CREATE TABLE profile
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    profile_picture VARCHAR(250),
+    biography VARCHAR(250),
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE tag
+(
+    id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE message_tags
+(
+    message_id BIGINT NOT NULL,
+    tag_id     BIGINT NOT NULL,
+    PRIMARY KEY (message_id, tag_id),
+    FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE
+);
